8d5d8c0afaa254f55e70ab168258e9ddb7540291
silvaannekely4 <silvaannekely4@gmail.com>
2025-08-22 10:01:41 -0300

    Add files via upload

---
+++ MyClass (6).java
@@ -0,0 +1,36 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class MyClass {
+  public static void main(String args[]) {
+    int x=10;
+    int y=25;
+    int z=x+y;
+
+    System.out.println("Sum of x+y = " + z);
+  }
+}Atividade 01
+- Simulando um sistema escolar, crie um algoritmo
+em Java que receba do aluno as 8 notas anuais. O algoritmo deve
+calcular as médias bimestrais, as médias semestrais, e a média
+final.
+Ao final dos cálculos, o algoritmo deve apresentar os resultados de
+forma clara. Por Exemplo:
+Práticas1º
+1:Bimestre: 6.6
+2:Bimestre: 8.0
+3:emestre: 6.5
+4:Bimestre: 8.0
+5:Semestre: 8.5
+-Média Final:10.
